4d20c282679f5202af3c0cbda53868c8075c06a2
Andrei Tumbar <andreitumbar@gmail.com>
2019-12-03 13:15:00 -0500

    Add lab 6 and idea project

--- 110/lab6/bitlogic.c
+++ 110/lab6/bitlogic.c
@@ -1,4 +1,59 @@
+// ===========================================================
 //
-// Created by atuser on 10/3/19.
+// Lab6_BitLogic.cpp
+// Description: Bit Logic
+// Name: Andrei Tumbar, Barak Binyamin
+// Date: 10/3/19
+// Class: CMPE-110
+// Section: 6 5:00PM
 //
+// ===========================================================
 
+#include "stdio.h"
+
+void print_result(unsigned char c) {
+    // Binary
+    int c_binary = c;
+
+    for (int i = 0; i < 8; i++) {
+        unsigned int truth = c_binary & ((int)1 << 7);
+        if (truth)
+            printf("1");
+        else
+            printf("0");
+        c_binary <<= 1;
+    }
+
+    printf(" 0x%02X", c);
+    printf("\n");
+    printf("\n");
+}
+
+int main() {
+	unsigned char A=0x33;
+	unsigned char B=0x4F;
+	unsigned char result;
+	char g = 0x1000;
+	printf("%X\n", g);
+
+	//Initial value A, B
+	printf("A= 0x%X, B= 0x%X \n", A, B);
+
+	// calculate ~A and then print it
+
+    print_result(A);
+    print_result(B);
+    print_result(~A);
+    print_result(A&B);
+    print_result(A|B);
+    print_result(A^B);
+    print_result(A << 1);
+    print_result(B >> 3);
+    print_result(A | 0x88);
+    print_result(A & (B | 0x88));
+
+	//continue with the rest of the equations in the exercise
+	//(i.e. calculate result and print it, for A&B, then A|B, etc.
+
+	return 0;
+}
--- 110/lab6/bitregister.c
+++ 110/lab6/bitregister.c
@@ -1,4 +1,33 @@
+// ===========================================================
 //
-// Created by atuser on 10/3/19.
+// Lab6_Register.cpp
+// Description: AMBA Register
+// Name: <team member names here>
+// Date: <today's date here>
+// Class: CMPE-110
+// Section: <Lab: section, day, and time here>
 //
+// ===========================================================
 
+#include "stdio.h"
+
+int main() {
+    unsigned char amba=0;
+
+    //initial value of the amba register
+    printf("After initialization, amba = 0x%X\n",amba);
+
+    //set 'Mode bit' and 'Enable bit' high
+    amba = amba | ((1 << 7) | (1 << 6)); //you need to change this line!
+    printf("After mode and enable bits set, amba = 0x%X\n",amba);
+
+    //also set the 'prescale' bits so 'clock divide by' is 16
+    amba = amba | (1 << 2); //you need to change this line!
+    printf("After setting prescale bits, amba = 0x%X\n",amba);
+
+    //clear the 'Mode bit', leaving other bits alone
+    amba = amba &(~(1 << 6)); //you need to change this line!
+    printf("After clearing the mode bit, amba = 0x%X\n",amba);
+
+    return 0;
+}
